55afcec41793aa4c9601a0ab7d0662cc2fd7d52b
Tyson Ngo <tyson_ngo@hotmail.com>
2017-01-19 03:02:20 -0500

    added index.js

---
+++ index.js
@@ -0,0 +1,34 @@
+var Discord = require("discord.js");
+
+var mybot = new Discord.Client();
+
+mybot.on("message", function(message) {
+	if (message.server.id === process.env.DISCORD_SERVER){
+		if (/^@everyone$/i.test(message.content)){
+		    var members = message.server.members;
+		    var memberIds = [];
+		    var msgs = [""];
+		    for (var key in members){
+		    	if (!isNaN(key)){
+		    		memberIds.push(members[key].id);
+		    	}
+		    }
+		    memberIds.forEach(function(id){
+		    	if (msgs[msgs.length-1].length+3+id.length <= 2000){
+		    		msgs[msgs.length-1] += '<@'+id+'>';
+		    	}else{
+		    		msgs.push('<@'+id+'>');
+		    	}
+			});
+			msgs.forEach(function(msg){
+				// X is filler
+				mybot.sendMessage(message,msg+"X".repeat(2000-msg.length),function(error,message){
+					console.log(error);
+					mybot.deleteMessage(message);
+				});
+			});
+		}
+	}
+});
+
+mybot.loginWithToken(process.env.DISCORD_TOKEN);
